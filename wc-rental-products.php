f0c6f3e40686153f34f365725f964edfb0024d1c
Mike Maltby <mike@ingersollrentall.ca>
2021-04-12 21:51:46 -0400

    add general tab and inventory tab to simple_rental products in admin

--- wc-rental-products.php
+++ wc-rental-products.php
@@ -142,12 +142,16 @@ class WC_Rental_Products_Plugin {
 
     ?>
     <script type='text/javascript'>
-      jQuery( document ).ready( function() {
-        //for General Tab
-        jQuery( '.options_group.pricing' ).addClass( 'show_if_simple_rental' ).show();
+      jQuery(document).ready(function () {
+        //for Price tab
+        jQuery('.product_data_tabs .general_tab').addClass('show_if_simple_rental').show();
+        jQuery('#general_product_data .pricing').addClass('show_if_simple_rental').show();
         //for Inventory tab
         jQuery('.inventory_options').addClass('show_if_simple_rental').show();
-      })
+        jQuery('#inventory_product_data ._manage_stock_field').addClass('show_if_simple_rental').show();
+        jQuery('#inventory_product_data ._sold_individually_field').parent().addClass('show_if_simple_rental').show();
+        jQuery('#inventory_product_data ._sold_individually_field').addClass('show_if_simple_rental').show();
+      });
     </script>
     <?php
 
